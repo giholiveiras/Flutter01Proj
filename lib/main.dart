7f0f5ddb3c05a21497c2f93961c1dac90c814530
giholiveiras <132359809+giholiveiras@users.noreply.github.com>
2024-03-14 16:29:47 -0300

    Projeto Flutter

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter2'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+
+    });}
+  void _decrementCounter() {
+    setState(() {
+
+      _counter--;
+    });}
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Sua contagem de toque:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+            ElevatedButton(onPressed: _incrementCounter, child: const Icon(Icons.add_circle_outline_sharp)),
+            ElevatedButton(onPressed: _decrementCounter, child: const Icon(Icons.remove_circle_outline_sharp)),
+          ],
+        ),
+        ],
+      ),
+    ),
+    );
+  }
+}
